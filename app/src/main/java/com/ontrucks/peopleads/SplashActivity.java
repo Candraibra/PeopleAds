cfd7c37c434cde1615b4ad66f910c9691a6ad75f
Candraibra <canisanie@gmail.com>
2019-09-10 10:36:18 +0700

    splash activity

--- app/src/main/java/com/ontrucks/peopleads/SplashActivity.java
+++ app/src/main/java/com/ontrucks/peopleads/SplashActivity.java
@@ -3,18 +3,24 @@ package com.ontrucks.peopleads;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //fullscreen request
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         Thread thread = new Thread() {
             public void run() {
                 try {
-                    sleep(3000);
+                    sleep(1500);
                 }catch (InterruptedException e) {
                     e.printStackTrace();
                 }finally {
