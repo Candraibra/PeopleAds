cb9eaa0e88097e085ff03e250c806f7c6f1962a2
Candraibra <canisanie@gmail.com>
2019-09-12 15:05:26 +0700

    home layout -> card view

--- app/src/main/java/com/ontrucks/peopleads/LoginActivity.java
+++ app/src/main/java/com/ontrucks/peopleads/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.ontrucks.peopleads;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,25 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView btnSignUp = findViewById(R.id.btnSignUp);
+        Button verifyOtp = findViewById(R.id.verifyOtp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+        verifyOtp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }
