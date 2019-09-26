160bb7b2a06496fa7c1319b91c24b050134b31ff
Candraibra <canisanie@gmail.com>
2019-09-26 17:05:40 +0700

    fragment

--- app/src/main/java/com/ontrucks/peopleads/CartFragment.java
+++ app/src/main/java/com/ontrucks/peopleads/CartFragment.java
@@ -1,10 +1,12 @@
 package com.ontrucks.peopleads;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +26,7 @@ public class CartFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cart, container, false);
     }
