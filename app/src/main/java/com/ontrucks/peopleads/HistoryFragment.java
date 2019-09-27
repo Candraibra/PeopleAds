7e11fe244fe2c40482b2822fdec7113a13812eb4
Candraibra <canisanie@gmail.com>
2019-09-27 15:36:33 +0700

    shimmer add

--- app/src/main/java/com/ontrucks/peopleads/HistoryFragment.java
+++ app/src/main/java/com/ontrucks/peopleads/HistoryFragment.java
@@ -2,12 +2,15 @@ package com.ontrucks.peopleads;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 
 /**
@@ -15,6 +18,7 @@ import android.view.ViewGroup;
  */
 public class HistoryFragment extends Fragment {
 
+    private ShimmerFrameLayout mShimmerViewContainer;
 
     public HistoryFragment() {
         // Required empty public constructor
@@ -25,7 +29,14 @@ public class HistoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mShimmerViewContainer = view.findViewById(R.id.shimmerContainer);
+       mShimmerViewContainer.startShimmer();
     }
 
 }
